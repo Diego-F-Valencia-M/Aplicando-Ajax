6ec8ae8d45c856ce0f2a96ff377bce65152dbdc7
Daniel Turriago <daniel@turriago.com>
2022-03-08 16:22:38 -0500

    update to fetch

--- ajax.js
+++ ajax.js
@@ -1,3 +1,22 @@
-
+// URLS
 const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon/?limit=151";
-const URL_POST = "https://jsonplaceholder.typicode.com/posts";
+const JSON_POST = "https://jsonplaceholder.typicode.com/posts";
+
+// DOM
+const container = document.querySelector("#container");
+const btn = document.querySelector("#button");
+const form = document.querySelector("#form");
+
+// FUNCIONES
+const capitalize = (str) => {
+  const lower = str.toLowerCase();
+  return str.charAt(0).toUpperCase() + lower.slice(1);
+}
+
+//LISTENERS
+btn.addEventListener("click", () => {
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
